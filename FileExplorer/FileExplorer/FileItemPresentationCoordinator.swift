337577751e51109830a44faa808a30dca2fa7ffa
Przemysław Sz <przemyslaw.szurmak@mobica.com>
2019-04-04 13:23:57 +0200

    send file when using sharing feature

--- FileExplorer/FileExplorer/FileItemPresentationCoordinator.swift
+++ FileExplorer/FileExplorer/FileItemPresentationCoordinator.swift
@@ -79,8 +79,7 @@ final class FileItemPresentationCoordinator {
 
 extension FileItemPresentationCoordinator: ActionsViewControllerDelegate {
     func actionsViewControllerDidRequestShare(_ controller: ActionsViewController) {
-        let activityItem = UIActivityItemProvider(placeholderItem: item.url)
-        let activityViewController = UIActivityViewController(activityItems: [activityItem], applicationActivities: nil)
+        let activityViewController = UIActivityViewController(activityItems: [item.url], applicationActivities: nil)
         navigationController?.present(activityViewController, animated: true, completion: nil)
     }
 
